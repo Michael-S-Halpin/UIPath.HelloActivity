bc4c6e4bcb17e50394945f5c2c6a3c7dc545aca8
Michael Halpin <mhalpin@gmx.com>
2022-08-11 22:36:35 -0400

    Cleaned up resources.

--- UIPath.HelloActivity/UIPath.HelloActivity.Activities/Properties/Resources.Designer.cs
+++ UIPath.HelloActivity/UIPath.HelloActivity.Activities/Properties/Resources.Designer.cs
@@ -61,15 +61,6 @@ namespace UiPath.HelloActivity.Activities.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Authentication.
-        /// </summary>
-        public static string Authentication_Category {
-            get {
-                return ResourceManager.GetString("Authentication_Category", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Common.
         /// </summary>
         public static string Common_Category {
@@ -120,15 +111,6 @@ namespace UiPath.HelloActivity.Activities.Properties {
         public static string InputOutput_Category {
             get {
                 return ResourceManager.GetString("InputOutput_Category", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Options.
-        /// </summary>
-        public static string Options_Category {
-            get {
-                return ResourceManager.GetString("Options_Category", resourceCulture);
             }
         }
         
@@ -354,51 +336,6 @@ namespace UiPath.HelloActivity.Activities.Properties {
         public static string TestScope_TestString_DisplayName {
             get {
                 return ResourceManager.GetString("TestScope_TestString_DisplayName", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Specifies the amount of time (in milliseconds) to wait for the activity to run before an error is thrown. The default value is 60000 (1 minute)..
-        /// </summary>
-        public static string Timeout_Description {
-            get {
-                return ResourceManager.GetString("Timeout_Description", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Timeout (milliseconds).
-        /// </summary>
-        public static string Timeout_DisplayName {
-            get {
-                return ResourceManager.GetString("Timeout_DisplayName", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to The activity timed out and was canceled..
-        /// </summary>
-        public static string Timeout_Error {
-            get {
-                return ResourceManager.GetString("Timeout_Error", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Please provide a value either for {0} or {1}, but not both..
-        /// </summary>
-        public static string ValidationExclusiveProperties_Error {
-            get {
-                return ResourceManager.GetString("ValidationExclusiveProperties_Error", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Please provide values for both {0} and {1}..
-        /// </summary>
-        public static string ValidationOverloadGroup_Error {
-            get {
-                return ResourceManager.GetString("ValidationOverloadGroup_Error", resourceCulture);
             }
         }
         
