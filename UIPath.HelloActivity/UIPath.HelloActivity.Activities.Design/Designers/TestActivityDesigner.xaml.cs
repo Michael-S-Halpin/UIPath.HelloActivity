5d979928e7fad6370c8451bce4d0956335f4de7e
Michael Halpin <mhalpin@gmx.com>
2022-08-16 19:38:40 -0400

    Updated references to enums.

--- UIPath.HelloActivity/UIPath.HelloActivity.Activities.Design/Designers/TestActivityDesigner.xaml.cs
+++ UIPath.HelloActivity/UIPath.HelloActivity.Activities.Design/Designers/TestActivityDesigner.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Windows;
+using UIPath.HelloActivity.Enums;
 using UiPath.Shared.Activities.Design.Controls;
 
 namespace UiPath.HelloActivity.Activities.Design.Designers;
