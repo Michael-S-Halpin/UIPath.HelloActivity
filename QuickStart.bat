421a2bca231013b007c9327cc1c83d5d4d8aa696
Michael Halpin <mhalpin@gmx.com>
2022-08-15 22:36:48 -0400

    Adding QuickStart.bat

---
+++ QuickStart.bat
@@ -0,0 +1,24 @@
+@echo off
+powershell -Command "(gc README.md) -replace 'UIPath.HelloActivity', '%1' | Out-File -encoding ASCII README.md
+powershell -Command "(gc UIPath.HelloActivity.sln) -replace 'UIPath.HelloActivity', '%1' | Out-File -encoding ASCII UIPath.HelloActivity.sln
+powershell -Command "(gc UIPath.HelloActivity\UIPath.HelloActivity.Activities\UIPath.HelloActivity.Activities.csproj) -replace 'UIPath.HelloActivity', '%1' | Out-File -encoding ASCII UIPath.HelloActivity\UIPath.HelloActivity.Activities\UIPath.HelloActivity.Activities.csproj
+powershell -Command "(gc UIPath.HelloActivity\UIPath.HelloActivity.Activities\Properties\Resources.Designer.cs) -replace 'UIPath.HelloActivity', '%1' | Out-File -encoding ASCII UIPath.HelloActivity\UIPath.HelloActivity.Activities\Properties\Resources.Designer.cs
+powershell -Command "(gc UIPath.HelloActivity\UIPath.HelloActivity.Activities.Design\DesignerMetadata.cs) -replace 'UIPath.HelloActivity', '%1' | Out-File -encoding ASCII UIPath.HelloActivity\UIPath.HelloActivity.Activities.Design\DesignerMetadata.cs
+powershell -Command "(gc UIPath.HelloActivity\UIPath.HelloActivity.Activities.Design\UIPath.HelloActivity.Activities.Design.csproj) -replace 'UIPath.HelloActivity', '%1' | Out-File -encoding ASCII UIPath.HelloActivity\UIPath.HelloActivity.Activities.Design\UIPath.HelloActivity.Activities.Design.csproj
+powershell -Command "(gc UIPath.HelloActivity\UIPath.HelloActivity.Activities.Design\Designers\ScopeActivityDesigner.xaml) -replace 'UIPath.HelloActivity', '%1' | Out-File -encoding ASCII UIPath.HelloActivity\UIPath.HelloActivity.Activities.Design\Designers\ScopeActivityDesigner.xaml
+powershell -Command "(gc UIPath.HelloActivity\UIPath.HelloActivity.Activities.Design\Designers\ScopeActivityDesigner.xaml.cs) -replace 'UIPath.HelloActivity', '%1' | Out-File -encoding ASCII UIPath.HelloActivity\UIPath.HelloActivity.Activities.Design\Designers\ScopeActivityDesigner.xaml.cs
+powershell -Command "(gc UIPath.HelloActivity\UIPath.HelloActivity.Activities.Design\Properties\Resources.Designer.cs) -replace 'UIPath.HelloActivity', '%1' | Out-File -encoding ASCII UIPath.HelloActivity\UIPath.HelloActivity.Activities.Design\Properties\Resources.Designer.cs
+cd UIPath.HelloActivity\UIPath.HelloActivity.Activities
+rename UIPath.HelloActivity.Activities.csproj %1.Activities.csproj
+cd ..\UIPath.HelloActivity.Activities.Design
+rename UIPath.HelloActivity.Activities.Design.csproj %1.Activities.Design.csproj
+cd ..\..
+rename UIPath.HelloActivity.sln %1.sln
+cd UIPath.HelloActivity
+rename UIPath.HelloActivity.Activities %1.Activities
+rename UIPath.HelloActivity.Activities.Design %1.Activities.Design
+cd ..
+rename UIPath.HelloActivity %1
+cd ..
+rename UIPath.HelloActivity %1
+@echo on
